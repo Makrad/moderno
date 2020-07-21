e80f9af917c5f9b46090c2bd8ee9085a2b0e40ae
Makrad <makst2007@gmail.com>
2020-07-21 17:33:09 +0300

    added product-slider and product

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,14 @@
 $(function(){
-
+    $(".rate-star").rateYo({
+        rating:5,
+        starWidth:"12px",
+        readOnly:true,
+    });
+    $('.product-slider__inner').slick({
+        dots:true,
+        arrows:false,
+        slickToShow:4,
+        slidesToScroll:4,
+    });
+    var mixer = mixitup('.products__inner-box')
 });
